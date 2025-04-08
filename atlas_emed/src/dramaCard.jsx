cc7516f0744318525d0fc14f46fa509b713b28c8
gi47huwi <andreas.w.wagner@fau.de>
2025-04-08 18:31:20 +0200

    added db conversion

--- atlas_emed/src/dramaCard.jsx
+++ atlas_emed/src/dramaCard.jsx
@@ -23,7 +23,6 @@ function DramaCard({
             .then(response => response.json())
             .then(data => {
                 setDrama(data);
-                console.log("Drama data:", data);
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
